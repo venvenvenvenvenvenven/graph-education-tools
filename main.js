edd3701452aa36d17275e5c89bf986f5ebc80d1b
venvenvenvenvenvenven <142343685+venvenvenvenvenvenven@users.noreply.github.com>
2024-06-13 03:04:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -128,7 +128,7 @@ function draw() {
       editEdge: 'Редактировать ребро',
       addDescription: 'Кликните в свободное место, чтобы добавить новую вершину.',
       edgeDescription: 'Кликните на вершину и протяните ребро к другой вершине, чтобы соединить их.',
-      editEdgeDescription: 'Click on the control points and drag them to a node to connect to it.', // кластеры не используются
+      editEdgeDescription: 'Кликните на контрольные точки и протяните их к вершине, чтобы присоединить к ней ребро., // кластеры не используются
       createEdgeError: 'Cannot link edges to a cluster.',
       deleteClusterError: 'Clusters cannot be deleted.',
       editClusterError: 'Clusters cannot be edited.'}
